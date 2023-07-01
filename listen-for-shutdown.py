f2d383a45862a1d4b33d695ba959f800d06d9159
Allan Taylor <allan.taylor.pi@gmail.com>
2023-07-01 17:10:03 +1200

    Add a delay for shutdown

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
-
+# CONFIG
+PIN = 3
+HOLD_TIME = 1 #seconds
 
 import RPi.GPIO as GPIO
 import subprocess
+from time import perf_counter
 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
-
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+GPIO.setup(PIN, GPIO.IN)
+while True:
+    GPIO.wait_for_edge(PIN, GPIO.FALLING)
+    start = perf_counter()
+    while perf_counter()<start+HOLD_TIME:
+        if GPIO.input(PIN) == GPIO.HIGH:
+            break
+    else:
+        subprocess.call(['shutdown', '-h', 'now'], shell=False)
