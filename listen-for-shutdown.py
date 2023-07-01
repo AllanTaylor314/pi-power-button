252b8bbf06262bade29e9257da7387eacae0301b
Allan Taylor <allan.taylor.pi@gmail.com>
2023-07-01 17:27:17 +1200

    Change perf_counter to perf_counter_ns

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # CONFIG
 PIN = 3
-HOLD_TIME = 1 #seconds
+HOLD_TIME = 1000 # milliseconds
 
 import RPi.GPIO as GPIO
 import subprocess
-from time import perf_counter
+from time import perf_counter_ns
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.IN)
 while True:
     GPIO.wait_for_edge(PIN, GPIO.FALLING)
-    start = perf_counter()
-    while perf_counter()<start+HOLD_TIME:
+    start = perf_counter_ns()
+    while perf_counter_ns()<start+HOLD_TIME*1_000_000:
         if GPIO.input(PIN) == GPIO.HIGH:
             break
     else:
